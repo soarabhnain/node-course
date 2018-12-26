ea5979d27cb63be1d136131268d60e2343844ae9
Nain <335210@cognizant.com>
2018-12-26 18:49:31 +0530

    second

---
+++ rest-routing/RestfullBlogApp/app.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//Routes
+
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(newBlog);
+            res.redirect("/blogs");
+        }
+    })
+})
+
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("show", {blog:foundBlog});
+        }
+    });
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("edit", {blog:foundBlog});
+        }
+    });
+});
+
+app.put("/blogs/:id", function(req,res){
+    
+})
+
+
+
+app.listen(3000, function(){
+    console.log("Server has started");
+});
