c0a00044d81a2e684b202395cf6303369addf1d0
Nain <335210@cognizant.com>
2019-01-04 17:52:01 +0530

    Till YelpCamp Clean Up

---
+++ YelpCamp/v8/routes/campgrounds.js
@@ -0,0 +1,53 @@
+
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+
+router.get("/", function(req, res){
+    //res.render("campgrounds", {campgrounds: campgrounds});
+
+    console.log(req.user);
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds/index", {campgrounds: allCampgrounds, currentUser: req.user});
+        }
+    })
+
+});
+
+router.get("/new", isLoggedIn, function(req, res){
+    res.render('campgrounds/new');
+})
+
+router.post("/", isLoggedIn, function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name:name, image:image, description: desc};
+    Campground.create(newCampground);
+    res.redirect("/campgrounds");
+});
+
+router.get("/:id", function(req, res){
+
+    Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+    
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } else{
+        res.redirect("/login");
+    }
+}
+
+module.exports = router;
